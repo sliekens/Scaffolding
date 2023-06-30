568f7ac4c6741d5cddd77408df0cc4ace1a2e396
Deep Choudhery <decho@microsoft.com>
2023-06-30 14:45:22 -0700

    cleaned up project structure more

--- src/Shared/Scaffolders/dotnet-scaffold-commands/ScaffolderSettings.cs
+++ src/Shared/Scaffolders/dotnet-scaffold-commands/ScaffolderSettings.cs
@@ -1,10 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.DotNet.Scaffolding.Shared.Spectre;
+using Spectre.Console.Cli;
 
 namespace Microsoft.DotNet.Tools.Scaffold.Commands
 {
-    public class DefaultCommandSettings
+    public class DefaultCommandSettings : CommandSettings
     {
         public bool? Interactive { get; set; }
 
